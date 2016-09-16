209a362130c6278ef935d86eb34cbeeab9b57147
Jose Sanchez-Garcia <josebsan96@gmail.com>
2016-09-15 23:46:01 -0700

    Completed Assignment 1

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -8,39 +8,40 @@
 
 import Foundation
 class CalculatorBrain {
-    private enum Op {
-        case Operand(Double)
-        case UnaryOperation(String, Double -> Double)
-        case BinaryOperation(String, (Double, Double) -> Double)
+    fileprivate enum Op {
+        case operand(Double)
+        case unaryOperation(String, (Double) -> Double)
+        case binaryOperation(String, (Double, Double) -> Double)
     }
-    private var opStack = [Op]()
+    fileprivate var opStack = [Op]()
     
-    private var knownOps = [String: Op]()
+    fileprivate var knownOps = [String: Op]()
     
     // This is our initilizer for our Op
     init(){
-        knownOps["×"] = Op.BinaryOperation("×") {$0 * $1}
-        knownOps["÷"] = Op.BinaryOperation("÷") {$1 / $0}
-        knownOps["+"] = Op.BinaryOperation("+") {$0 + $1}
-        knownOps["−"] = Op.BinaryOperation("−") {$1 - $0}
-        knownOps["√"] = Op.UnaryOperation("√") { sqrt($0) }
+        knownOps["×"] = Op.binaryOperation("×") {$0 * $1}
+        knownOps["÷"] = Op.binaryOperation("÷") {$1 / $0}
+        knownOps["+"] = Op.binaryOperation("+") {$0 + $1}
+        knownOps["−"] = Op.binaryOperation("−") {$1 - $0}
+        knownOps["√"] = Op.unaryOperation("√") { sqrt($0) }
+        //knownOps["π"] = Op.unaryOperation("π") { M_PI }
     }
     
-    private func Evaluate(ops: [Op]) -> (results: Double?, remainingOps: [Op]){
+    fileprivate func Evaluate(_ ops: [Op]) -> (results: Double?, remainingOps: [Op]){
         if !ops.isEmpty{
             var remainingOps = ops // This gives us the ability to edit the array
             let op = remainingOps.removeLast()
             switch op {
-            case .Operand(let operand):
+            case .operand(let operand):
                 return (operand, remainingOps)
                 
-            case .UnaryOperation(_, let operation):
+            case .unaryOperation(_, let operation):
                 let operandEvaluation = Evaluate(remainingOps)
                 if let operand = operandEvaluation.results {
                     
                     return (operation(operand), operandEvaluation.remainingOps)
                 }
-            case .BinaryOperation(_, let operation):
+            case .binaryOperation(_, let operation):
                 let op1Evaluation = Evaluate(remainingOps)
                 
                 if let operand1 = op1Evaluation.results {
@@ -63,12 +64,12 @@ class CalculatorBrain {
         return result
     }
     
-    func pushOperand(operand: Double) -> Double?{
-        opStack.append(Op.Operand(operand))
+    func pushOperand(_ operand: Double) -> Double?{
+        opStack.append(Op.operand(operand))
         return evaluate()
     }
     
-    func performOperation(symbol: String) -> Double?{
+    func performOperation(_ symbol: String) -> Double?{
         if let operation = knownOps[symbol] {
             opStack.append(operation)
         }
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,53 +11,147 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var history: UILabel!
     
-    var userInTheMiddleOfTypingNumber: Bool = false;
+    var userInTheMiddleOfTypingNumber: Bool = false
     
-    // This is connection between controller and model
-    var brain = CalculatorBrain()
-    
-    @IBAction func appendDigit(sender: UIButton) {
+    @IBAction func appendDigit(_ sender: UIButton) {
         // Let is considered to be as a "const" for java or C++
         let digit = sender.currentTitle
         
         if userInTheMiddleOfTypingNumber {
             display.text = display.text! + digit!
+            
         } else
         {
             display.text = digit!
             userInTheMiddleOfTypingNumber = true
         }
     }
+    // This is connection between controller and model
+    //var brain = CalculatorBrain()
+    /*
+    @IBAction func appendDigit(_ sender: UIButton) {
+     
+     
+    }
+*/
+    let x = M_PI // Value of Pi
 
+    @IBAction func Pi(_ sender: UIButton) {
+
+        userInTheMiddleOfTypingNumber = true
+        history.text = history.text! + ", π"
+        
+        if display.text != "0" {
+            Enter() // Push the current value into the stack
+            display.text = "\(x)" //set the label to pie (actual value)
+            Enter() // now push it into the stack
+        }
+        else {
+            display.text = "\(x)" // set the label to pie
+            Enter() // push into stack
+        }
+        
+    }
+    
+    // This function will be used to reset the contents of the display
+    // label and empty the stack.
+    @IBAction func clear(_ sender: UIButton) {
+        display.text = "0"
+        operandStack.removeAll()
+        userInTheMiddleOfTypingNumber = false
+        history.text = ""
+        counter = 1
+    }
+
+    // Create a variable that will keep track of when the
+    // user pressed this button
+    var dotPressed: Bool = false
+    @IBAction func dot(_ sender: UIButton) {
+        userInTheMiddleOfTypingNumber = true
+        // Check whether the button was pressed already
+        if dotPressed == false
+        {
+            display.text = display.text! + "."
+            dotPressed = true
+        }
+    }
+    
     // function used to take in operation options
-    @IBAction func operate(sender: UIButton) {
+
+    @IBAction func operate(_ sender: UIButton) {
         if userInTheMiddleOfTypingNumber {
             Enter()
         }
-        if let operation = sender.currentTitle {
-            if let result = brain.performOperation(operation){
-                displayValue = result
-            }
-            else{
-                displayValue = 0
-            }
+        let operation = sender.currentTitle!
+        
+        history.text = history.text! + ", " + operation
+        // Switch function for operation
+        switch operation {
+        case "×": performOperation {$0 * $1}
+        case "÷": performOperation {$1 / $0}
+        case "+": performOperation {$0 + $1}
+        case "−": performOperation {$1 - $0}
+        case "√": PerformOperation {sqrt($0) }
+        case "sin": PerformOperation { sin($0) }
+        case "cos": PerformOperation { cos($0) }
+        
+        default: break;
+        }
+    }
+    
+    // function to perform operation with two parameters
+    func performOperation(operation: (Double, Double) -> Double)
+    {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            
+            //history.text = display.text! + ", " + history.text!
+            Enter()
         }
     }
 
+    func PerformOperation(operation: (Double) -> Double){
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            Enter()
+        }
+    }
+    var counter = 1
+    // Creating a function for when they enter PerformOperation
     @IBAction func Enter() {
         userInTheMiddleOfTypingNumber = false;
-        brain.pushOperand(displayValue)
+        
+        print(operandStack)
+        if counter == 1
+        {
+            history.text = display.text!
+            counter = counter + 1
+        }
+        else
+        {
+            history.text = display.text! + ", " + history.text!
+        }
+        // Make sure we reset back the dotPressed function to false
+        dotPressed = false
+        // Push value into the operandStack
+        operandStack.append(displayValue)
+        //brain.pushOperand(displayValue)
+
     }
     
+    
+    var operandStack = Array<Double>()
     var displayValue: Double {
         get {
-            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+            return NumberFormatter().number(from: display.text!)!.doubleValue
         }
         set {
             display.text = "\(newValue)"
             userInTheMiddleOfTypingNumber = false
         }
     }
+    
 }
 
